d28c70f0a8ff86208fb854a73abf2228a8c602ec
Isaac-Tait <isaac@mountaintopcoding.com>
2021-07-25 12:40:11 -0700

    work on final stage of the math

--- kilometerTimeToMileTimeProj/kilometerTimeToMileTimeProj/main.cpp
+++ kilometerTimeToMileTimeProj/kilometerTimeToMileTimeProj/main.cpp
@@ -4,12 +4,17 @@
 int main()
 {
 	float seconds{}, decimalTime{};
+
 	int m{ userKmTime() };
 	int s{ userKmTime() };
 
 	seconds = (m * 60) + s;
 
-	decimalTime = seconds / 0.621371192 * 60;
+	decimalTime = seconds / 0.621371192 / 60;
+
+	//std::modf();
+
+	std::cout << "If your speed remained the same, your 1km split of " << m << " minutes " << s << " seconds" << ", would be a 1 mile split of " << decimalTime << '\n';
 
 	return decimalTime;
 }
