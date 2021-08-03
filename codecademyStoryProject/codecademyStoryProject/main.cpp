f2344cc32fe8bbc0c13d11d82be8288c081783ad
Isaac-Tait <isaac@mountaintopcoding.com>
2021-08-02 17:39:17 -0700

    add more storyline/plot

--- codecademyStoryProject/codecademyStoryProject/main.cpp
+++ codecademyStoryProject/codecademyStoryProject/main.cpp
@@ -6,13 +6,19 @@
 int main() {
 
 	std::string planetOne = "Planet Vanessa", planetTwo = "Planet Lyra", planetThree = "Planet Xing";
+	//Decision point one - which planet to land on.
 	int x = {};
+	//Story fork point one - which planet was selected?
 	int y = {};
+	//Decision point two - each planet has two options upon selection int a records the users decision
+	int a = {};
 
-	std::cout << "You are the pilot of USS Starship Polkadot. Your destination was Alpha Centauri. However, there was an issue with your flight computer and you had to drop off of the light sail or risk the ship imploding.\n";
-	std::cout << "After dumping the light sail you can see that your ship is close to three planets. The only thing that is known is their names.\n";
+	int z = {};
 
-	std::cout << "They are planets Vanessa, Lyra, and Xing. Which one will you choose?\n";
+	std::cout << "Excuse me pilot this is the fligh computer of the USS Starship Polkadot. I am prematuraly awakening you from cryosleep.\nYour destination was Alpha Centauri. However, there was an issue with our telemetry.\nI had no choice but to drop off of the light sail or risk the ship imploding.\n";
+	std::cout << "After dumping the light sail I can detect three nearbyt planets. As of right now the only thing that I know is their names.\n\n";
+
+	std::cout << "They are planets Vanessa, Lyra, and Xing. Which one will you choose to land on? Regretably, we do not have much fuel left so we cannot stay in space.\n";
 	std::cout << "Planet Vanessa = 1   Planet Lyra = 2    Planet Xing = 3\n";
 	std::cin >> x;
 
@@ -45,8 +51,66 @@ int main() {
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	std::cout << "Ahh screw it " << "0!!\n";
 
-	if (y = 1) {
+	if (y == 1) {
+		std::cout << "The atmosphere of " << planetOne << " is much thinner than I anticpated. Entry was smooth, however we overshot my intended landing zone. I see two possibilities." << '\n';
+		std::cout << "One we land on a watery substance that looks like the ocean with a thin fog veneer...\n";
+		std::cout << "Or we land in a primevial looking forest with lots of tall rock formations dripping with waterfalls...\n";
+		std::cout << "What is your choice? 1 = Ocean   2 = Forest\n";
 
+		std::cin >> a;
+
+		if (a == 1) {
+			std::cout << "What?! Seriously, you want to land in the ocean? You are crazy... but it is your funeral I guess... Wait it is mine too. Argh why did the engineers who designed me not give me the freedom to override a human's dumb decisions!?\n";
+			z = 1;
+		} else if (a == 2) {
+			std::cout << "Okay, the forest is the best choice out of the two possibilities. It is too bad we overshot my intended landing zone though - it looked like a space port where we could refeul... Alas\n";
+			z = 2;
+		}
+		else {
+			std::cout << "You should never read this because it is the flight computers personal journal recording all the dumb decisions my pilots have made over the years.... Initiating self destruct. Goodbye forever.\n";
+		}
 	}
+	else if (y == 2) {
+		std::cout << "Warning warning warning! The atmospher of " << planetTwo << " was too dense and I could not detect the asteroid belt. We have sustained severe damage." << '\n';
+		std::cout << "I do not know if we can land... What do you want to do?" << "\n";
+		std::cout << "Bail out of the shuttle with your escape pod or ride the flaming wreckage to the planets surface?\n";
+		std::cout << "What is your choice? 1 = Bail    2 = Stay in the shuttle\n";
 
+		std::cin >> a;
+
+		if (a == 1) {
+			std::cout << "While I understand your logic I regret that you have chosen to leave me behind after all we have been through... Arming the escape pod now!" << "\n";
+			z = 3;
+		}
+		else if (a == 2) {
+			std::cout << "Thank you for not abandoning me in my darkest hour. I think I can still land this flaming hulk on that giant mushroom below us. It appears to be soft and squishy.\n";
+			z = 4;
+		}
+		else {
+			std::cout << "You should never read this because it is the flight computers personal journal recording all the dumb decisions my pilots have made over the years.... Initiating self destruct. Goodbye forever.\n";
+		}
+	}
+	else if (y == 3) {
+		std::cout << "Ummm I do not know how to tell you this but it would appear that " << planetThree << " is inhabited by some sort of alien race. We are being approached by an unknown space craft." << '\n';
+		std::cout << "Our weapon systems are fully functional and armed or we could just hope for the best...\n";
+		std::cout << "What is your choice? 1 = Shoot down the space craft    2 = Hold on and hope for the best...\n";
+
+		std::cin >> a;
+
+		if (a == 1) {
+			std::cout << "Good choice! As my pappy used to say 'Shoot first, ask questions later' (he was a salty Marine, what can I say?).\n";
+			z = 5;
+		}
+		else if (a == 2) {
+			std::cout << "Sometimes you just need to kick a little butt. However, since you obviously do not agree I regret to inform you that they have locked on to our ship with their tractor beam. I wish I could tell you I told you so but we do not have that much time...\n";
+			z = 6;
+		}
+		else {
+			std::cout << "You should never read this because it is the flight computers personal journal recording all the dumb decisions my pilots have made over the years.... Initiating self destruct. Goodbye forever.\n";
+		}
+	}
+	else {
+		std::cout << "You should never read this unless there is a problem with the code...\n";
+	}
+	std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 }
