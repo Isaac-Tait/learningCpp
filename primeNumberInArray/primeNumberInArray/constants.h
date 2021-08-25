52854d57dba53d67c8b52ff469e740c5f5ec934b
Isaac-Tait <isaac@mountaintopcoding.com>
2021-08-24 17:53:25 -0700

    my brain hurts

--- primeNumberInArray/primeNumberInArray/constants.h
+++ primeNumberInArray/primeNumberInArray/constants.h
@@ -5,7 +5,7 @@
 
 namespace constants {
 
-	constexpr int randomOne[10] = { 1,3,5,7,9,12,13,15,18,23 };
+	constexpr int randomOne[11] = { 1,2,3,5,7,9,12,13,15,18,23 };
 	constexpr int randomTwo[12] = { 2,4,6,8,11,13,17,22,43,1,317,822 };
 
 }
--- primeNumberInArray/primeNumberInArray/main.cpp
+++ primeNumberInArray/primeNumberInArray/main.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include "constants.h"
-#include <cmath>
-
-//#define ENABLE_DEBUG
 
 bool isPrime(int x) 
 {
 	if (x < 2)
-		std::cout << x << " is not prime, rather it is a composite number.\n";
+	{
+		std::cout << x << " is not prime.\n";
+	}
+	else if (x == 3 || x == 2) 
+	{
+		std::cout << x << " is prime.\n";
+	}
+	else if (x % 2 == 0) 
+	{
+		std::cout << x << " is not prime.\n";
+	}
 
-		for (int i = 2; i <= sqrt(x); ++i) 
-		{
-			if ((x%i) == 0)
-				std::cout << x << " is not prime, rather it is a composite number.\n";
+	for (int j = 2; j <= sqrt(x); ++j) 
+	{
+		if (x % j == 0) {
+			std::cout << x << " is not prime\n";
+			break;
 		}
-		std::cout << x << " is prime!!!\n";
+		std::cout << x << " is prime\n";
+	}
 
-		return 3;
+	return 0;
 }
 
-int main() {
-
-	for (int i{ 0 }; i <= 9; ++i) 
+int main() 
+{
+	for (int i{ 0 }; i <= 10; ++i) 
 	{
 		isPrime(constants::randomOne[i]);				
 	}	
