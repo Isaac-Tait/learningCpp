f1c59855fb3ed0246c4c191f35f9f10b601acfd8
Isaac-Tait <isaac@mountaintopcoding.com>
2021-09-03 17:13:03 -0700

    playing around some more with vectors

--- playingWithVectors/playingWithVectors/Source.cpp
+++ playingWithVectors/playingWithVectors/Source.cpp
@@ -3,8 +3,23 @@
 
 int main() {
 
-	std::vector<int> x(4);
+	std::vector<int> x{8,17,23,38};
 
-	x.at[0] = 8;
+	std::cout << "The number at position 1 is: " << x.at(0) << '\n'; // Returns 8
 
+	x.at(0) = 9;
+	std::cout << "The number at position 1 has been updated to: " << x.at(0) << '\n'; // Returns 9
+	
+	x.push_back(15);
+	std::cout << "The number " << x.at(4) << " has been added to the end of the vector." << '\n'; // Returns 15
+
+	std::cout << "The length of the vector is: " << x.size() << '\n';
+	
+	x.resize(6);
+
+	std::cout << "The length of the vector has been updated. It is now: " << x.size() << '\n';
+	for (int i : x)
+		std::cout << i << ' ';
+
+	return 0;
 }
