a9bdcf8ab68a95977dbacbeb6baac883364b70a9
Isaac-Tait <isaac@mountaintopcoding.com>
2021-09-23 21:22:32 -0700

    still working on it

--- iterateAnArray/iterateAnArray/Source.cpp
+++ iterateAnArray/iterateAnArray/Source.cpp
@@ -2,7 +2,20 @@
 
 int main() {
 	constexpr int array[]{ 4, 6, 7, 3, 8, 2, 1, 9, 5 };
-	int iterate{ static_cast<int>(std::size(array)) };
 
+	int x{};
+	std::cout << "Enter a number between 1 and 9\n";
+	std::cin >> x;
 
+	if (x <= 9 && x > 0) {
+		for (int count{ 0 }; count < static_cast<int>(std::size(array)); count++)
+		{
+			std::cout << array[count] << '\n';
+		}
+	}
+	else {
+		int x{};
+		std::cout << "Enter a number between 1 and 9\n";
+		std::cin >> x;
+	}
 }
