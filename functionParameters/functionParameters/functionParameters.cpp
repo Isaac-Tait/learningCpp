75342f7aa48a818c8e5e0c6e77dd5814165385c6
Isaac Tait <isaac@mountaintopcoding.com>
2022-08-05 20:12:13 -0400

    function parameter 2nd half and quiz

--- functionParameters/functionParameters/functionParameters.cpp
+++ functionParameters/functionParameters/functionParameters.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
 
+//Quiz 4 & 5
+int doubleNumber(int x)
+{
+    return x + x;
+}
+
+int main()
+{
+    std::cout << "Enter an integer then press any key" << "\n";
+
+    int userNumber{ };
+
+    std::cin >> userNumber;
+
+    std::cout <<  "Your number doubled is " << doubleNumber(userNumber) << "\n";
+
+    return 0;
+}
+
+//Having some fun with the second half of the lesson
+/*
+int add(int x, int y)
+{
+    return x + y;
+}
+
+int multiply(int a, int z)
+{
+    return a * z;
+}
+
+int main()
+{
+    std::cout << add(4, multiply(9,7)) << '\n';
+
+    std::cout << multiply(add(9,8), multiply(3,7)) << '\n';
+
+    return 0;
+}
+*/
+
+/*
 int getValueFromUser()
 {
     std::cout << "Enter an integer please: " << "\n";
@@ -22,3 +64,29 @@ int main()
 
     return 0;
 }
+*/
+
+//Simplified code from first attempt above. Removed one line in int main()
+/*
+int getValueFromUser()
+{
+    std::cout << "Enter an integer please: " << "\n";
+    int input{ };
+    std::cin >> input;
+
+    return input;
+}
+
+void printDouble(int num)
+{
+    std::cout << num << " doubled is: " << num * 2 << "\n";
+}
+
+int main()
+{
+    printDouble(getValueFromUser());
+
+    return 0;
+}
+*/
+//https://www.learncpp.com/cpp-tutorial/introduction-to-function-parameters-and-arguments/
